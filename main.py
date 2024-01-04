a618a26b27e3c06dc58d98f2dd76456d89505529
Soprat <57324077+Soprat@users.noreply.github.com>
2024-01-04 18:50:23 +0200

    Update and rename maxim.py to main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = App().creating()
 @app.on_message(filters.channel &
                 filters.chat(ids.donors))
 async def copy_message_to_tech(message):
-    ''' '''
+    '''Copies message from donor's channels to tech'''
     await app.copy_message(
         chat_id=ids.tech,
         from_chat_id=message.chat.id,
@@ -23,7 +23,7 @@ async def copy_message_to_tech(message):
                 filters.chat(ids.tech) &
                 filters.reply)
 async def send(message):
-    ''' '''
+    '''Sends messages to targets from tech channel'''
     for chat_id in ids.targets:
         try:
             await app.copy_message(
@@ -37,15 +37,17 @@ async def send(message):
 
 
 @app.on_message(filters=filters.command(['add_donors']) &
+                filters.chat(ids.tech) &
                 filters.reply)
 async def add_donors(message):
     ''' At command 'add_targets' with replying to message,
         bot will add channel's id's in replied  to 'Targets' in database'''
     await app.send_message(
-        chat_id=message.reply_to_message.chat.id, 
+        chat_id=message.reply_to_message.chat.id,
         text=db.send("Donors", message.reply_to_message.text))
 
 @app.on_message(filters=filters.command(['add_targets']) &
+                filters.chat(ids.tech) &
                 filters.reply)
 async def add_targets(message):
     """At command 'add_targets' with replying to message,
@@ -56,6 +58,7 @@ async def add_targets(message):
 
 
 @app.on_message(filters=filters.command(['join']) &
+                filters.chat(ids.tech) &
                 filters.reply)
 async def join_channels(message):
     """At command 'join' with replying to message, bot will
