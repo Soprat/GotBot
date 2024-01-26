5e6996062f0b6a8af227fc86512164a822ea3950
Soprat <Qwuker666@gmail.com>
2024-01-26 16:40:02 +0200

    Major update

--- database.py
+++ database.py
@@ -8,31 +8,26 @@ class Database:
         self.file_ids = []
         self.caption = None
 
-        connection = psycopg2.connect(database="aichrrdb",
-                                      host="surus.db.elephantsql.com",
-                                      user="aichrrdb",
-                                      password="cghCrs8TTwRTpxXsBT_yD7FaIaBYMAey",
-                                      port=5432)
-        self.cursor = connection.cursor()
+        self.connection = psycopg2.connect(database="aichrrdb",
+                                           host="surus.db.elephantsql.com",
+                                           user="aichrrdb",
+                                           password="cghCrs8TTwRTpxXsBT_yD7FaIaBYMAey",
+                                           port=5432)
+        self.cursor = self.connection.cursor()
 
-        message_columns_list = ['caption', 'file0_id', 'file1_id', 'file2_id',
-                                'file3_id', 'file4_id', 'file5_id', 'file6_id',
-                                'file7_id', 'file8_id', 'file9_id', 'media_group']
-
-        self.message_columns = ''
-        for x in message_columns_list:
-            self.message_columns += x
-
-    def send(self, file_ids: list[list[PhotoSize]], caption: str, media_group: int) -> str | Exception:
-        file_ids = file_ids[0]
-        values_list = [caption, file_ids[0].file_id, file_ids[1].file_id, file_ids[2].file_id,
-                       file_ids[3].file_id, file_ids[4].file_id, file_ids[5].file_id, file_ids[6].file_id,
-                       file_ids[7].file_id, file_ids[8].file_id, file_ids[9].file_id, media_group]
-        values = ''
-        for x in values_list:
-            values += x
+    def send(self, caption: str, file_ids: list[str], file_formats: list[str]) -> tuple | Exception:
+        values_list = (caption, *(file_ids[:10]), *(file_formats[:10]))
         try:
-            self.cursor.execute(f"INSERT INTO message ({self.message_columns}) VALUES ({values})")
+            self.cursor.execute(f"INSERT INTO message VALUES {values_list}")
+            self.connection.commit()
+            return values_list
+        except Exception as e:
+            return e
+
+    def get(self, caption: str) -> tuple | Exception:
+        try:
+            self.cursor.execute(f"SELECT * FROM message WHERE caption = '{caption}'")
+            values = self.cursor.fetchone()
             return values
         except Exception as e:
             return e
--- main.py
+++ main.py
@@ -11,7 +11,9 @@ from aiogram.types import (
     Message,
 )
 from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.utils.media_group import MediaGroupBuilder
 from aiogram_album.lock_middleware import LockAlbumMiddleware
+from aiogram_album import AlbumMessage
 
 from database import Database
 
@@ -20,6 +22,7 @@ storage = MemoryStorage()
 bot_token: str = '6739572759:AAHQepgsUc9wDP-uYGwUMDtScs-GNR26x7M'
 dp = Dispatcher()
 router = Router(name=__name__)
+base = Database()
 
 donors: list[int] = [-4093149059]
 technical: int = -1002085744420
@@ -27,38 +30,71 @@ technical: int = -1002085744420
 bot = Bot(bot_token, parse_mode="HTML")
 
 
-@dp.message(F.media_group_id)
-async def copy_message_to_tech(message: Message):
+def create_media_group(caption,  file_types: list[str], file_ids) -> list[InputMediaAudio
+                                                                          | InputMediaPhoto
+                                                                          | InputMediaVideo
+                                                                          | InputMediaDocument]:
+    media_group = MediaGroupBuilder(caption=caption)
+    for file_id in file_ids:
+        match file_types[file_ids.index(file_id)-1]:
+            case 'photo':
+                media_group.add(type='photo', media=file_id)
+            case 'video':
+                media_group.add(type='photo', media=file_id)
+            case 'audio':
+                media_group.add(type='audio', media=file_id)
+            case 'document':
+                media_group.add(type='document', media=file_id)
+    return media_group.build()
+
+
+@dp.message(F.media_group_id & F.chat.id.in_(donors))
+async def copy_message_to_tech(message: AlbumMessage):
     """This handler will receive a complete album of any type
     and copy from donors to tech"""
-    print(message.caption, message.media_group_id, "\n\n", [m.photo for m in message])
-    print([m.photo for m in message][0])
+    file_ids = []
+    file_formats = []
+    for m in message:
+        content_type = [m.content_type.value][0]
+        file_ids.append(str(m.photo[0].file_id))
+        file_formats.append(content_type)
+
+    response = base.send(caption=message.caption,
+                         file_ids=file_ids,
+                         file_formats=file_formats)
+    print(response)
+    await message.reply(text=str(response))
     await message.copy_to(technical)
-    return
 
 
 @dp.message(Command("send"),
-            F.reply_to_message,
+            F.reply_to_message &
             F.reply_to_message.media_group_id)
-async def send_message_to_targets(message: Message):
+async def send_message_to_targets(message: AlbumMessage):
     """from tech to targets"""
     targets: list[int] = [-4093149059]
     await message.answer('sending')
+    data = base.get(caption=f'{message.reply_to_message.caption}')
+    index = []
+    for i in ['photo', 'video', 'audio', 'document']:
+        if i in data:
+            index.append(data.index(i))
+    first_type = min(index)
+    file_types = data[first_type:first_type+first_type-1]
+
+    media_group = create_media_group(caption=data[0], file_types=file_types, file_ids=data[1:first_type])
 
     for channel in targets:
-        # await message.reply_to_message.copy_to(chat_id=channel)
         try:
-            # noinspection PyUnresolvedReferences
-            await message.reply_to_message.message_id.forward(chat_id=channel)
-        except AttributeError:
-            pass
-        # await message.copy_to(chat_id=channel)
+            await bot.send_media_group(chat_id=channel, media=media_group)
+        except Exception as e:
+            print(e)
 
 
 @router.message(Command('info'))
 async def get_message_info(message):
-    # await bot.send_media_group(message.chat.id, media=files)
-    await message.answer(message)
+    """get info about message on which replied /info"""
+    await message.reply_message.answer(message)
 
 
 async def main() -> None:
