b47c88899f1dbaaaca7ae365355b5b1be8b13e85
Soprat <Qwuker666@gmail.com>
2024-02-20 14:57:22 +0200

    Update to v3.0

--- main.py
+++ main.py
@@ -7,100 +7,152 @@ from aiogram.types import (
     InputMediaDocument,
     InputMediaPhoto,
     InputMediaVideo,
-    TelegramObject,
     Message,
 )
-from aiogram.fsm.storage.memory import MemoryStorage
+
 from aiogram.utils.media_group import MediaGroupBuilder
 from aiogram_album.lock_middleware import LockAlbumMiddleware
 from aiogram_album import AlbumMessage
 
+from dataclasses import dataclass
 from database import Database
+from coloring import ColoredFormatter
+from dotenv import dotenv_values
+import logging
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler()
+
+log_format = '[%(asctime)s]:%(levelname)-7s:%(message)s'
+time_format = '%H:%M:%S'
+formatter = ColoredFormatter(log_format, datefmt=time_format)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel('DEBUG')
 
 DEFAULT_DELAY = 0.6
-storage = MemoryStorage()
-bot_token: str = ''
+
+config = dotenv_values('.env')
 dp = Dispatcher()
-router = Router(name=__name__)
 base = Database()
-
-donors: list[int] = []
-technical: int = 
-
-bot = Bot(bot_token, parse_mode="HTML")
+bot = Bot(config["BOT_TOKEN"])
+donors = base.get_all_donors()
 
 
-def create_media_group(caption,  file_types: list[str], file_ids) -> list[InputMediaAudio
-                                                                          | InputMediaPhoto
-                                                                          | InputMediaVideo
-                                                                          | InputMediaDocument]:
+def create_media_group(
+        caption: str,
+        file_types: list[str],
+        file_ids: list[str]) -> list[InputMediaAudio
+                                     | InputMediaPhoto
+                                     | InputMediaVideo
+                                     | InputMediaDocument]:
     media_group = MediaGroupBuilder(caption=caption)
     for file_id in file_ids:
-        match file_types[file_ids.index(file_id)-1]:
-            case 'photo':
-                media_group.add(type='photo', media=file_id)
-            case 'video':
-                media_group.add(type='video', media=file_id)
-            case 'audio':
-                media_group.add(type='audio', media=file_id)
-            case 'document':
-                media_group.add(type='document', media=file_id)
+        file_type = file_types[file_ids.index(file_id)]
+        media_group.add(type=file_type, media=file_id)
     return media_group.build()
 
 
-@dp.message(F.media_group_id & F.chat.id.in_(donors))
-async def copy_message_to_tech(message: AlbumMessage):
-    """This handler will receive a complete album of any type
-    and copy from donors to tech"""
-    file_ids = []
-    file_formats = []
+@dataclass
+class DataHandle:
+    """Handles merge command."""
+    cluster_donors: list[str] = None
+    cluster_targets: list[str] = None
+    black_text: str = None
+    set_text: str = None
+
+    def __init__(self, text: str):
+        replaces = ['d', 't', 'del', 'set']
+
+        text = text.replace('/merge ', ' ')
+        text = text.replace('/edit_text ', ' ')
+        text = text.split(' --')[1:]
+
+        for i in text:
+            temp = replaces.index(i.split()[0])
+            if temp < 2:
+                i = i.split()[1:]
+                res = [j.replace(',', '') for j in i]
+
+                if temp == 0:
+                    self.cluster_donors = res
+                elif temp == 1:
+                    self.cluster_targets = res
+
+            elif temp == 2:
+                self.black_text = i.replace('del', '', 1)
+            elif temp == 3:
+                self.set_text = i.replace('set', '', 1)
+
+
+@dp.message(Command('chat_id'), F.reply)
+async def chat_id(message: Message):
+    logger.info('Chat info message appeared!')
+    await message.answer(str(message.reply_to_message.forward_from_chat.id))
+
+
+@dp.message(Command('cluster_info'))
+async def chat_id(message: Message):
+    """/cluster_info <donor>"""
+    logger.info('Message cluster_info appeared!')
+    donor = message.text.split()[-1]
+    base.get_cluster(base.get_cluster_id(donor))
+
+
+@dp.message(F.chat.id.in_(donors), F.photo)
+async def copy_to_targets(message: AlbumMessage):
+    """copies message from donor to targets"""
+    logger.info('Message to copy appeared!')
+
+    text = message.caption or message.text
+    cluster_id = base.get_cluster_id(message.chat.id)
+    cluster = base.get_cluster(cluster_id=cluster_id)
+    caption = text.replace(cluster[0][3].lstrip(), cluster[0][4].lstrip())
+
+    attachments = []
+    media_group = MediaGroupBuilder(caption=caption)
     for m in message:
-        content_type = [m.content_type.value][0]
-        file_ids.append(str(m.photo[0].file_id))
-        file_formats.append(content_type)
+        for media in {'photo', 'video', 'audio', 'document'}:
+            attachment = getattr(m, media, None)
+            if attachment:
+                attachments.append({'type': media, 'file_id': attachment[-1].file_id})
+    for attachment in attachments:
+        media_group.add(type=attachment['type'], media=attachment['file_id'])
 
-    response = base.send(caption=message.caption,
-                         file_ids=file_ids,
-                         file_formats=file_formats)
-    print(response)
-    await message.reply(text=str(response))
-    await message.copy_to(technical)
-
-
-@dp.message(Command("send"),
-            F.reply_to_message &
-            F.reply_to_message.media_group_id)
-async def send_message_to_targets(message: AlbumMessage):
-    """from tech to targets"""
-    targets: list[int] = [-4093149059]
-    await message.answer('sending')
-    data = base.get(caption=f'{message.reply_to_message.caption}')
-    index = []
-    for i in ['photo', 'video', 'audio', 'document']:
-        if i in data:
-            index.append(data.index(i))
-    first_type = min(index)
-    file_types = data[first_type:first_type+first_type-1]
-
-    media_group = create_media_group(caption=data[0], file_types=file_types, file_ids=data[1:first_type])
-
-    for channel in targets:
-        try:
-            await bot.send_media_group(chat_id=channel, media=media_group)
-        except Exception as e:
-            print(e)
+    for cluster_ in cluster:
+        await bot.send_media_group(chat_id=cluster_[2], media=media_group.build())
 
 
-@router.message(Command('info'))
-async def get_message_info(message):
-    """get info about message on which replied /info"""
-    await message.reply_message.answer(message)
+@dp.message(Command('change_text'), F.chat.id == config["TECHNICAL"])
+async def change_text(message: Message):
+    """/change_text --d <donor> --del <text_to_del> --set <text_to_set>"""
+    data = DataHandle(message.text)
+    cluster_id = base.get_cluster_id(data.cluster_donors[0])
+    response = base.edit_text(cluster_id=cluster_id,
+                              new_text_to_set=data.set_text,
+                              new_text_to_del=data.black_text)
+    logger.info(response)
+
+
+@dp.message(Command('merge'), F.chat.id == config["TECHNICAL"])
+async def merge(message: Message):
+    """/merge --d <donors> --t <targets> --del <text_to_del> --set <texts_to_set>"""
+    logger.info('Merge command handled')
+    data = DataHandle(message.text)
+    max_cluster_id = base.get_max_id()
+    print(f'donors: {data.cluster_donors}, targets = {data.cluster_targets},'
+          f' text_to_delete = {data.black_text}, text_to_set = {data.set_text}')
+    response = base.create_cluster(cluster_id=max_cluster_id+1,
+                                   targets=data.cluster_targets,
+                                   donors=data.cluster_donors,
+                                   text_to_delete=data.black_text,
+                                   text_to_set=data.set_text)
+    logger.info(response)
 
 
 async def main() -> None:
     # Initialize Bot instance with a default parse mode which will be passed to all API calls
-    print("Started")
-
+    logger.info('Started')
     await dp.start_polling(bot)
 
 
